676b27f4e77333dd13d6e935a62214a949a935b5
ghrfh01 <m17362952972@163.comom>
2020-11-14 11:29:10 +0800

    测试jenkins

---
+++ src/main/java/com/wfd360/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.wfd360.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ */
+@RestController
+@RequestMapping("/")
+public class HelloController {
+    /**
+     * 获取首页
+     */
+    @RequestMapping("/index")
+    public String index(String test) {
+
+        return "succss"+test;
+    }
+}
